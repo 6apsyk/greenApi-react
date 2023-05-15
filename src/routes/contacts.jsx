f2287f5c082ab6cb4a4bafe912f12b21a44af7cd
IliaKuryndin <6apsyk@mail.ru>
2023-05-15 22:58:23 +0300

    проверка deploy

--- src/routes/contacts.jsx
+++ src/routes/contacts.jsx
@@ -1,13 +1,13 @@
 import {useEffect, useRef, useState} from "react";
 import {useParams} from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 import {Chat} from "../components/Chat/Chat";
 
 export default function Contact() {
     const {contactId} = useParams();
 
-    const idInstance = JSON.parse(localStorage.getItem("config"))?.idInstance;
-    const apiTokenInstance = JSON.parse(localStorage.getItem("config"))?.apiTokenInstance;
+    // const idInstance = JSON.parse(localStorage.getItem("config"))?.idInstance;
+    // const apiTokenInstance = JSON.parse(localStorage.getItem("config"))?.apiTokenInstance;
 
     const timerRef = useRef(null);
 
@@ -17,54 +17,52 @@ export default function Contact() {
 
     const onSubmit = async () => {
         clearTimeout(timerRef.current);
-        const url = `https://api.green-api.com/waInstance${idInstance}/SendMessage/${apiTokenInstance}`;
-        await axios({
-            url,
-            method: "post",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            data: {
-                chatId: `${contactId}@c.us`,
-                message: text,
-            },
-        });
+        // const url = `https://api.green-api.com/waInstance${idInstance}/SendMessage/${apiTokenInstance}`;
+        // await axios({
+        //     url,
+        //     method: "post",
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        //     data: {
+        //         chatId: `${contactId}@c.us`,
+        //         message: text,
+        //     },
+        // });
         setText("");
         onReload();
     };
 
     const onReload = async () => {
-        clearTimeout(timerRef.current);
-        const url = `https://api.green-api.com/waInstance${idInstance}/ReceiveNotification/${apiTokenInstance}`;
-        const {data} = await axios({
-            url,
-            method: "get",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-
-        if (data !== null) {
-            const url2 = `https://api.green-api.com/waInstance${idInstance}/DeleteNotification/${apiTokenInstance}/${data?.receiptId}`;
-            await axios({
-                url: url2,
-                method: "delete",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                data: {},
-            });
-
-            if (
-                data.body.typeWebhook === "incomingMessageReceived" ||
-                data.body.typeWebhook === "outgoingAPIMessageReceived"
-            ) {
-                setMessages(prev => prev.concat(data.body));
-            }
-            timerRef.current = setTimeout(() => onReload(), 2000);
-        } else {
-            timerRef.current = setTimeout(() => onReload(), 15000);
-        }
+        // clearTimeout(timerRef.current);
+        // const url = `https://api.green-api.com/waInstance${idInstance}/ReceiveNotification/${apiTokenInstance}`;
+        // const {data} = await axios({
+        //     url,
+        //     method: "get",
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        // });
+        // if (data !== null) {
+        //     const url2 = `https://api.green-api.com/waInstance${idInstance}/DeleteNotification/${apiTokenInstance}/${data?.receiptId}`;
+        //     await axios({
+        //         url: url2,
+        //         method: "delete",
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //         },
+        //         data: {},
+        //     });
+        //     if (
+        //         data.body.typeWebhook === "incomingMessageReceived" ||
+        //         data.body.typeWebhook === "outgoingAPIMessageReceived"
+        //     ) {
+        //         setMessages(prev => prev.concat(data.body));
+        //     }
+        //     timerRef.current = setTimeout(() => onReload(), 2000);
+        // } else {
+        //     timerRef.current = setTimeout(() => onReload(), 15000);
+        // }
     };
 
     const handleKeyPress = e => {
